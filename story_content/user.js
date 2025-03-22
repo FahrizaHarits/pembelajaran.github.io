50d8f8b3dabb7620854b5850656d10df11c3fe3d
fahriza ahmad harits <fahrizaharits123@gmail.com>
2025-03-22 08:35:38 +0700

    test

---
+++ story_content/user.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6OrWrbLhWjS":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById( “bgsong”);
+audio. src="musik.mp3" ;
+audio.load;
+audio.play() ;
+}
+
